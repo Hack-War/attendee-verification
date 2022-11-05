df669e8de6baa00d853a5f7b91e620b0995b746b
sooryakriz11 <sooryakriz111@gmail.com>
2022-11-06 04:04:54 +0530

    Dashboard header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from "./components/Header/Header";
 import Login from "./pages/Login"
 import Signup from "./pages/Signup";
 
 function App() {
+  const location = useLocation()
+  console.log(location.pathname)
   return (
     <div className="font-Poppins">
+      {location.pathname === '/login' || location.pathname === '/signup' ? <></> : <Header />}
       <Routes>
         {/* Login Route */}
         <Route path="/login" element={<Login />} />
