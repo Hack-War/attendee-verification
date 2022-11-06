79b926670841c4b0489c2cb38861a5ea92df4bd7
sooryakriz11 <sooryakriz111@gmail.com>
2022-11-06 09:00:58 +0530

    New event check in form completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Form from "./pages/Form";
 import Login from "./pages/Login"
 import Signup from "./pages/Signup";
 
@@ -10,6 +11,7 @@ function App() {
     <div className="font-Poppins">
       {location.pathname === '/login' || location.pathname === '/signup' ? <></> : <Header />}
       <Routes>
+        <Route path="/event/new" element={<Form />} />
         {/* Login Route */}
         <Route path="/login" element={<Login />} />
         {/* Signup Route */}
